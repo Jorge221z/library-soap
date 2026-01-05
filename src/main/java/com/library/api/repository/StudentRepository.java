9ef213caa54c8401ad6813ae6858aa3ba600e48c
Jorge221z <jorgemunozcast12@gmail.com>
2026-01-05 14:37:34 +0100

    Fix method implementation

--- src/main/java/com/library/api/repository/StudentRepository.java
+++ src/main/java/com/library/api/repository/StudentRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
 
-  StudentEntity findById(long id);
+  // This will be managed by the default JPA methods
 
 }
