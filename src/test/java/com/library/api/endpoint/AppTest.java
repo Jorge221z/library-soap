83e812803d627cceec79b9a7d7430255895ce387
Jorge221z <jorgemunozcast12@gmail.com>
2026-01-10 19:44:59 +0100

    Migrate base test to JUnit 5

--- src/test/java/com/library/api/endpoint/AppTest.java
+++ src/test/java/com/library/api/endpoint/AppTest.java
@@ -1,38 +1,18 @@
 package com.library.api.endpoint;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    // JUnit 5 does not extend from TestCase
+    // There are no neccesary constructors or suite() methods
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testApp() {
+        assertTrue(true);
     }
 }
